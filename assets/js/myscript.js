cbee35c04eb0948bde11e6ea47353f1e83279f1b
Daniel Zins <danzins@Daniels-Mac-mini.local>
2025-12-14 22:09:10 -0600

    Removed header and footer sections. Removed extra JQuery code relating to header and footer sections. Made the table horizontal scrollable when breakpoint is at 767px

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,25 +1,3 @@
-// Slide toggle Profile Menu
-$('.profile-dropdown').hide(); //this hides the list initially
-
-$('.profile-dropdown-menu').click(function(){
-    $(this).next(".profile-dropdown").slideToggle();
-});
-
-// Hamburger Menu Links Level 1
-$('.menu-link-level1').click(function(){
-    $(this).next().slideToggle();
-
-    if($('.hamburger-submenu-level2:visible').length > 1) {
-        $('.hamburger-submenu-level2:visible').hide();
-        $(this).next().show();
-    }
-});
-
-// Hamburger Menu Links Level 2
-$('.submenu-level-2').click(function(){
-    $(this).next(".hamburger-submenu-level3").slideToggle();
-});
-
 // Slide Toggle Sub Table
 $('.chevron-toggle').click(function(){
     $(this).parent().parent().next('tr').children('td').children('.table-container__inner-table-container').slideToggle();
